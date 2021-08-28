c6bddd52e929828740fa8469d46a1ee581f2fe6b
Z_BM <58821444+Zentraxus@users.noreply.github.com>
2021-08-28 14:16:39 +0200

    Create Medium-Encryption.py

---
+++ Medium-Encryption.py
@@ -0,0 +1,30 @@
+def encryption(s):
+    # Write your code here
+    chars_without_spaces = [i for i in s if i != " "]
+    rows_number = int((len(chars_without_spaces)**0.5)//1)
+    if rows_number**2 >= len(chars_without_spaces):
+        columns_number = rows_number
+    elif rows_number*(rows_number+1) >= len(chars_without_spaces):
+        columns_number = rows_number + 1
+    else:
+        columns_number = rows_number + 1
+        rows_number += 1
+    rows = []
+    for k in range(rows_number):
+        row = ""
+        for i in range(columns_number*k,columns_number*k+columns_number):
+            try:
+                row += chars_without_spaces[i]
+            except IndexError:
+                pass
+        rows.append(row)
+    final_string = ""
+    for i in range(columns_number):
+        string = ""
+        for k in range(rows_number):
+            try:
+                string += rows[k][i]
+            except IndexError:
+                continue
+        final_string += (string + " ")
+    return final_string
